d9c1d401e2e3749fc478d322a48e848dc2a1694b
Daniil Smolyakov <danonofficial@gmail.com>
2022-07-16 13:57:47 +0200

    BasicTile class with assets

---
+++ Assets/Tiles/ScriptsTile/BasicTile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class BasicTile : Tile
+{
+    public Sprite[] m_Sprites;
+    public Sprite m_Preview;
+    public override void RefreshTile(Vector3Int location, ITilemap tilemap)
+    {
+        for (int yd = -1; yd <= 1; yd++)
+            for (int xd = -1; xd <= 1; xd++)
+            {
+                Vector3Int position = new Vector3Int(location.x + xd, location.y + yd, location.z);
+                tilemap.RefreshTile(position);
+            }
+    }
+#if UNITY_EDITOR
+    // The following is a helper that adds a menu item to create a RoadTile Asset
+    [MenuItem("Assets/Create/2D/Tiles/BasicTile")]
+    public static void CreateBaseTile()
+    { 
+        string path = EditorUtility.SaveFilePanelInProject("Save Basic Tile", "New Basic Tile", "Asset", "Save Basic Tile", "Assets");
+        if (path == "")
+            return;
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<BasicTile>(), path);
+    }
+#endif
+}
