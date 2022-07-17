cce9d9b75704eb7ce8fe4c24c3cc019b4ced87b3
PointPisdez <wsxdora@mail.ru>
2022-07-17 21:48:00 +0300

    lsdkfn

--- Assets/MainMenu/MainMenuScripts.cs
+++ Assets/MainMenu/MainMenuScripts.cs
@@ -18,37 +18,37 @@ public class MainMenuScripts : MonoBehaviour
                 SceneManager.LoadScene("SampleScene");
                 break;
             case 2:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 1");
                 break;
             case 3:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 2");
                 break;
             case 4:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 3");
                 break;
             case 5:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 4");
                 break;
             case 6:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 5");
                 break;
             case 7:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 6");
                 break;
             case 8:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 7");
                 break;
             case 9:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 8");
                 break;
             case 10:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 9");
                 break;
             case 11:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 10");
                 break;
             case 12:
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("SampleScene 11");
                 break;
             default:
                 SceneManager.LoadScene("SampleScene");
