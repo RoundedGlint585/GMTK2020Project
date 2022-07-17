4f0824cbc413543e7076ad27eab5fbc0b42f5058
PointPisdez <wsxdora@mail.ru>
2022-07-17 18:11:07 +0300

    Add UI on level

---
+++ Assets/MainMenu/SceneScriptsUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScriptsUI : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void StepBack()
+    {
+
+    }
+}
